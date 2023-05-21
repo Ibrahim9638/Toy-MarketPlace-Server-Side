737715931804ff76f14de8a3dde7c335f8b840d4
Ibrahim <you@example.com>
2023-05-21 23:05:16 +0600

    final check

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ async function run() {
       res.send(result);
     });
 
-
+// Delete Api
    app.delete('/my-toys/:id', async(req, res)=>{
     const id = req.params.id;
     const query = { _id: new ObjectId(id) }
